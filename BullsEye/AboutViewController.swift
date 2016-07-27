ac9162dba09734b73dbf94769e655fb071130ca6
amrish036 <amrish.036@gmail.com>
2016-07-27 22:15:31 +1000

    be3 added aboutViewController

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Amrish Mahesh on 27/07/2016.
+//  Copyright © 2016 Amrish Mahesh. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
