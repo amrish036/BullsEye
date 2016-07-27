3866c3cfad4a0f3c3f160bb034a8823e5476cd34
amrish036 <amrish.036@gmail.com>
2016-07-27 17:12:30 +1000

    be1 added slider and lables

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -21,5 +21,18 @@ class ViewController: UIViewController {
   }
 
 
+  @IBAction func showAlert() {
+    let alert = UIAlertController(title: "Hello, World",
+                                  message: "This is my first app!",
+                                  preferredStyle: .Alert)
+    let action = UIAlertAction(title: "Awesome", style: .Default,
+                               handler: nil)
+    alert.addAction(action)
+    presentViewController(alert, animated: true, completion: nil)
+  }
+
+  @IBAction func sliderMoved(slider: UISlider) {
+    print("The value of the slider is now: \(slider.value)");
+  }
 }
 
